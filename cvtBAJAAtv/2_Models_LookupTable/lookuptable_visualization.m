f32c1ec38826e5f2efc10ac03158db103cb3483d
Veer Alakshendra <valakshe@mathworks.com>
2020-11-05 09:42:32 +0530

    Transmission changed as per BAJA competition
    Transmission changed as per BAJA competition

--- cvtBAJAAtv/2_Models_LookupTable/lookuptable_visualization.m
+++ cvtBAJAAtv/2_Models_LookupTable/lookuptable_visualization.m
@@ -3,7 +3,7 @@ cvtModel_data
 sim('cvtModel_data')
 
 %% Visualize the lookup table 
-plot(vehicle_speed,cvt_ratio,'r','LineWidth',2)
+plot(vehicle_speed,cvtr,'r','LineWidth',2)
 xlabel ('Vehicle Speed (km/hr)')
 ylabel ('CVT Ratio')
 title ('Lookup Table Visualization')
